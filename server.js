c3894b367b4523708fa41dd6a4e0d5940984fa15
Gustavr0 <gust040825@gmail.com>
2021-05-22 23:39:39 -0500

    critical error solution

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to gudemy application." });
+  res.json({ message: "Welcome to Youtuve api." });
 });
 
 // routes
